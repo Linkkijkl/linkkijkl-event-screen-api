3e081deead676ada48397c04f92b6c706024f4f2
kurkilmo <ilmo.kurki00@gmail.com>
2025-03-24 18:42:08 +0200

    Implement ical event endpoints

--- events.js
+++ events.js
@@ -6,22 +6,26 @@ const { LINKKI_EVENTS_URL, ALGO_EVENTS_URL } = require('./config');
 
 const router = express.Router();
 
+const getLinkkiUrl = () => {
+  const currentDate = new Date();
+  const twoMonthsFromNow = new Date();
+  twoMonthsFromNow.setMonth(twoMonthsFromNow.getMonth() + 2);
+  const baseUrl = `${LINKKI_EVENTS_URL}/c_g2eqt2a7u1fc1pahe2o0ecm7as@group.calendar.google.com/events`;
+  const params = {
+    calendarId: 'c_g2eqt2a7u1fc1pahe2o0ecm7as@group.calendar.google.com',
+    singleEvents: 'true',
+    timeZone: 'Europe/Helsinki',
+    timeMin: currentDate.toISOString(),
+    timeMax: twoMonthsFromNow.toISOString(),
+    orderBy: 'startTime',
+    key: 'AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs'
+  };
+  return `${baseUrl}?${new URLSearchParams(params).toString()}`;
+}
+
 router.get('/linkki', async (req, res) => {
   try {
-    const currentDate = new Date();
-    const twoMonthsFromNow = new Date();
-    twoMonthsFromNow.setMonth(twoMonthsFromNow.getMonth() + 2);
-    const baseUrl = `${LINKKI_EVENTS_URL}/c_g2eqt2a7u1fc1pahe2o0ecm7as@group.calendar.google.com/events`;
-    const params = {
-      calendarId: 'c_g2eqt2a7u1fc1pahe2o0ecm7as@group.calendar.google.com',
-      singleEvents: 'true',
-      timeZone: 'Europe/Helsinki',
-      timeMin: currentDate.toISOString(),
-      timeMax: twoMonthsFromNow.toISOString(),
-      orderBy: 'startTime',
-      key: 'AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs'
-    };
-    const url = `${baseUrl}?${new URLSearchParams(params).toString()}`;
+    const url = getLinkkiUrl()
     const data = await fetchData(url);
     res.send(data);
   } catch (err) {
@@ -45,7 +49,7 @@ router.get('/algo', async (req, res) => {
   }
 });
 
-router.get('/algo/ical', async (req, res) => {
+router.get('/ical/algo', async (req, res) => {
   try {
     const calendar = new ical.ICalCalendar()
     calendar.name('Algon Tapahtumat')
@@ -56,6 +60,9 @@ router.get('/algo/ical', async (req, res) => {
     const $ = cheerio.load(html);
     $('article.eventlist-event.eventlist-event--upcoming').each((i, el) => {
       const summary = $(el).find('a.eventlist-title-link').text();
+      const location = $(el).find('li.eventlist-meta-address').text().replace('(kartta)', '').trim()
+      let url = $(el).find('a.eventlist-title-link').attr('href');
+      url = ALGO_EVENTS_URL + url.replace('/tapahtumat', '')
       const date = $(el).find('time.event-date').attr('datetime');
       const startTime = $(el).find('time.event-time-localized-start').text()
       const endTime = $(el).find('time.event-time-localized-end').text()
@@ -65,13 +72,40 @@ router.get('/algo/ical', async (req, res) => {
       calendar.createEvent({
         summary,
         start,
-        end
+        end,
+        url,
+        location
       });
     });
 
     res.type('text/calendar').send(calendar.toString())
   } catch (err) {
     handleError(err, res);
+  }
+})
+
+router.get('/ical/linkki', async (req, res) => {
+  try {
+    const calendar = new ical.ICalCalendar()
+    calendar.name('Linkin Tapahtumat')
+    calendar.prodId('-')
+    const data = await fetchData(getLinkkiUrl())
+
+
+    data.items.forEach(item => {
+      calendar.createEvent({
+        summary: item.summary,
+        description: item.description,
+        start: item.start.dateTime || item.start.date,
+        end: item.end.dateTime,
+        allDay: !item.end.dateTime,
+        location: item.location
+      })
+    })
+
+    res.type('text/calendar').send(calendar.toString())
+  } catch (err) {
+    handleError(err, res)
   }
 })
 
